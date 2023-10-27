a7a8c1ad68013ac5e2074200ef54416a2f8ed19a
Aleksandr Zelenin <aleksandr@zelenin.me>
2023-10-28 01:36:14 +0300

    added api

--- oodle.go
+++ oodle.go
@@ -1,6 +1,8 @@
 package oodle
 
+import "C"
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,36 +13,82 @@ import (
 )
 
 const (
+	CompressorInvalid = -1
+	CompressorNone    = 3
+
+	// new
+	CompressorKraken    = 8
+	CompressorLeviathan = 13
+	CompressorMermaid   = 9
+	CompressorSelkie    = 11
+	CompressorHydra     = 12
+
+	// deprecated
+	CompressorBitKnit = 10
+	CompressorLZB16   = 4
+	CompressorLZNA    = 7
+	CompressorLZH     = 0
+	CompressoLZHLW    = 1
+	CompressorLZNIB   = 2
+	CompressorLZBLW   = 5
+	CompressorLZA     = 6
+
+	CompressorCount   = 14
+	CompressorForce32 = 0x40000000
+)
+
+const (
 	CompressionLevelNone      = 0
 	CompressionLevelSuperFast = 1
 	CompressionLevelVeryFast  = 2
 	CompressionLevelFast      = 3
 	CompressionLevelNormal    = 4
-	CompressionLevelOptimal1  = 5
-	CompressionLevelOptimal2  = 6
-	CompressionLevelOptimal3  = 7
-	CompressionLevelCount     = 8
+
+	CompressionLevelOptimal1 = 5
+	CompressionLevelOptimal2 = 6
+	CompressionLevelOptimal3 = 7
+	CompressionLevelOptimal4 = 8
+	CompressionLevelOptimal5 = 9
+
+	CompressionLevelHyperFast1 = -1
+	CompressionLevelHyperFast2 = -2
+	CompressionLevelHyperFast3 = -3
+	CompressionLevelHyperFast4 = -4
+
+	// aliases
+	CompressionLevelHyperFast = CompressionLevelHyperFast1
+	CompressionLevelOptimal   = CompressionLevelOptimal2
+	CompressionLevelMax       = CompressionLevelOptimal5
+	CompressionLevelMin       = CompressionLevelHyperFast4
+
+	CompressionLevelForce32 = 0x40000000
+	CompressionLevelInvalid = CompressionLevelForce32
 )
 
 const (
-	AlgoLZH         = 0
-	AlgoLZHLW       = 1
-	AlgoLZNIB       = 2
-	AlgoNone        = 3
-	AlgoLZB16       = 4
-	AlgoLZBLW       = 5
-	AlgoLZA         = 6
-	AlgoLZNA        = 7
-	AlgoKraken      = 8
-	AlgoLZQ1        = 8
-	AlgoMermaid     = 9
-	AlgoLZNIB2      = 9
-	AlgoBitKnit     = 10
-	AlgoSelkie      = 11
-	AlgoHydra       = 12
-	AlgoAkkorokamui = 12
-	AlgoLeviathan   = 13
-	AlgoCount       = 14
+	FuzzSafeNo  = 0
+	FuzzSafeYes = 1
+)
+
+const (
+	CheckCRCNo      = 0
+	CheckCRCYes     = 1
+	CheckCRCForce32 = 0x40000000
+)
+
+const (
+	VerbosityNone    = 0
+	VerbosityMinimal = 1
+	VerbositySome    = 2
+	VerbosityLots    = 3
+	VerbosityForce32 = 0x40000000
+)
+
+const (
+	DecodeThreadPhase1   = 1
+	DecodeThreadPhase2   = 2
+	DecodeThreadPhaseAll = 3
+	DecodeUnthreaded     = DecodeThreadPhaseAll
 )
 
 var (
@@ -53,7 +101,7 @@ var (
 
 var dllRe = regexp.MustCompile(dllName)
 
-func Compress(input []byte, algo int, compressionLevel int) ([]byte, error) {
+func Compress(input []byte, compressor int, compressionLevel int) ([]byte, error) {
 	dll, err := getDll()
 	if err != nil {
 		return nil, err
@@ -68,19 +116,31 @@ func Compress(input []byte, algo int, compressionLevel int) ([]byte, error) {
 	outputSize := inputSize * 2
 	output := make([]byte, outputSize)
 
+	var options uintptr = 0
+	var dictionaryBase uintptr = 0
+	var lrm uintptr = 0
+	var scratchMem uintptr = 0
+	var scratchSize uintptr = 0
+
 	r1, _, err := proc.Call(
-		uintptr(algo),
+		uintptr(compressor),
 		uintptr(unsafe.Pointer(&input[0])),
 		uintptr(inputSize),
 		uintptr(unsafe.Pointer(&output[0])),
 		uintptr(compressionLevel),
-		0,
-		0,
-		0,
+		options,
+		dictionaryBase,
+		lrm,
+		scratchMem,
+		scratchSize,
 	)
 
 	if err.(syscall.Errno) != 0 {
 		return nil, err
+	}
+
+	if r1 == 0 {
+		return nil, errors.New("compress failure")
 	}
 
 	data := make([]byte, r1)
@@ -103,28 +163,149 @@ func Decompress(input []byte, outputSize int64) ([]byte, error) {
 	inputSize := len(input)
 	output := make([]byte, outputSize)
 
-	_, _, err = proc.Call(
+	var decBufBase uintptr = 0
+	var decBufSize uintptr = 0
+	var fpCallback uintptr = 0
+	var callbackUserData uintptr = 0
+	var decoderMemory uintptr = 0
+	var decoderMemorySize uintptr = 0
+
+	r1, _, err := proc.Call(
 		uintptr(unsafe.Pointer(&input[0])),
 		uintptr(inputSize),
 		uintptr(unsafe.Pointer(&output[0])),
 		uintptr(outputSize),
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		3,
+		FuzzSafeNo,
+		CheckCRCNo,
+		VerbosityNone,
+		decBufBase,
+		decBufSize,
+		fpCallback,
+		callbackUserData,
+		decoderMemory,
+		decoderMemorySize,
+		DecodeThreadPhaseAll,
 	)
 
 	if err.(syscall.Errno) != 0 {
 		return nil, err
 	}
 
+	if r1 == 0 {
+		return nil, errors.New("decompress failure")
+	}
+
 	return output, nil
+}
+
+func DecompressChunk(input []byte, outputSize int64) ([]byte, error) {
+	dll, err := getDll()
+	if err != nil {
+		return nil, err
+	}
+
+	decoderCreateProc, err := dll.FindProc("OodleLZDecoder_Create")
+	if err != nil {
+		return nil, err
+	}
+
+	//var compressor uintptr = CompressorInvalid
+	var memory uintptr = 0
+	var memorySize uintptr = 0
+
+	r1, _, err := decoderCreateProc.Call(
+		-1,
+		uintptr(outputSize),
+		memory,
+		memorySize,
+	)
+
+	if err.(syscall.Errno) != 0 {
+		return nil, err
+	}
+
+	decodeSomeProc, err := dll.FindProc("OodleLZDecoder_DecodeSome")
+	if err != nil {
+		return nil, err
+	}
+
+	inputSize := len(input)
+	output := make([]byte, 20000)
+	decBuf := make([]byte, 20000)
+
+	var decBufPos uintptr = 0
+	var decBufferSize uintptr = uintptr(inputSize)
+	var decBufAvail = decBufferSize - decBufPos
+
+	r1, _, err = decodeSomeProc.Call(
+		r1,
+		uintptr(unsafe.Pointer(&output[0])),
+		uintptr(unsafe.Pointer(&decBuf[0])),
+		decBufPos,
+		decBufferSize,
+		decBufAvail,
+
+		uintptr(unsafe.Pointer(&input[0])),
+		uintptr(inputSize),
+		FuzzSafeNo,
+		CheckCRCNo,
+		VerbosityNone,
+		DecodeThreadPhaseAll,
+	)
+
+	if err.(syscall.Errno) != 0 {
+		return nil, err
+	}
+
+	if r1 == 0 {
+		return nil, errors.New("decompress failure")
+	}
+
+	return output, nil
+}
+
+func GetCompressionLevelName(compressionLevel int) (string, error) {
+	dll, err := getDll()
+	if err != nil {
+		return "", err
+	}
+
+	proc, err := dll.FindProc("OodleLZ_CompressionLevel_GetName")
+	if err != nil {
+		return "", err
+	}
+
+	r1, _, err := proc.Call(
+		uintptr(compressionLevel),
+	)
+
+	if err.(syscall.Errno) != 0 {
+		return "", err
+	}
+
+	return C.GoString((*C.char)(unsafe.Pointer(r1))), nil
+}
+
+func GetCompressorName(compressor int) (string, error) {
+	dll, err := getDll()
+	if err != nil {
+		return "", err
+	}
+
+	proc, err := dll.FindProc("OodleLZ_Compressor_GetName")
+	if err != nil {
+		return "", err
+	}
+
+	r1, _, err := proc.Call(
+		uintptr(compressor),
+	)
+
+	if err.(syscall.Errno) != 0 {
+		return "", err
+	}
+
+	return C.GoString((*C.char)(unsafe.Pointer(r1))), nil
 }
 
 var dllOnce struct {
